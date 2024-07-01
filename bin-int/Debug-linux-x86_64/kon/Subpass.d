d23adeb879c6c03c3aa7a6292f04bef037a4e9be
tomatoo <aidannwilhite@gmail.com>
2024-07-01 11:53:52 -0700

    fixed untracked files

--- bin-int/Debug-linux-x86_64/kon/Subpass.d
+++
@@ -1,3 +0,0 @@
-../bin-int/Debug-linux-x86_64/kon/Subpass.o: \
- src/kon/graphics/vulkan/pipeline/Subpass.cpp /usr/include/stdc-predef.h
-/usr/include/stdc-predef.h:
--- bin-int/Debug-windows-x86_64/kon/Subpass.d
+++
@@ -1,2 +0,0 @@
-../bin-int/Debug-windows-x86_64/kon/Subpass.o: \
- src/kon/graphics/vulkan/pipeline/Subpass.cpp
